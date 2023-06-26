33cec3baa829a9b97d1fe67501590cc52c89451c
Elrich-sam <rsam00103@gmail.com>
2023-06-26 18:39:57 +0000

    changes

--- base_app.py
+++ base_app.py
@@ -48,10 +48,10 @@ def main():
 		st.markdown(f"<style>{designs.read()}</style>", unsafe_allow_html=True)
 
 	logo = Image.open('Images/white.png')
-	#logo2 = Image.open('Images/gg.png')
+	logo2 = Image.open('Images/gg.png')
 	st.image(logo, use_column_width=True)
-	#st.image(logo2)
-	st.markdown(f'<img src="http://localhost:8501/media/6b4b9df26b5cb297db50888a170735f06acdcd490592b420e511ae81.png" alt="logo" class="logo-image">', unsafe_allow_html=True)
+	st.image(logo2)
+	#st.markdown(f'<img src="Images/gg.png" alt="logo" class="logo-image">', unsafe_allow_html=True)
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
 	
